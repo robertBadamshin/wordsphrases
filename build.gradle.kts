1b1a7311472da22f46c3043afc9aeba868a384b9
Badamshin Robert <r.badamshin@softpro.com>
2022-05-24 11:43:27 +0300

    remove gradle version duplication

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
 
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
         classpath("com.google.gms:google-services:4.3.10")
