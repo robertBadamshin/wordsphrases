0949a1484c3b42c751a6717bb1863f78c83f74bf
Badamshin Robert <r.badamshin@softpro.com>
2022-06-01 20:57:11 +0300

    fix build

--- android/stories-impl/src/main/java/com/app/wordsphrases/stories_impl/di/StoriesComponent.kt
+++ android/stories-impl/src/main/java/com/app/wordsphrases/stories_impl/di/StoriesComponent.kt
@@ -3,7 +3,6 @@ package com.app.wordsphrases.stories_impl.di
 import com.app.wordsphrases.core.AppComponent
 import com.app.wordsphrases.core.BaseWordsPhrasesApp.Companion.appComponent
 import com.app.wordsphrases.core.di.FeatureScope
-import com.app.wordsphrases.popup_translator_impl.di.DaggerStoriesComponent
 import com.app.wordsphrases.stories_impl.presentation.StoriesPresenter
 import dagger.Component
 
