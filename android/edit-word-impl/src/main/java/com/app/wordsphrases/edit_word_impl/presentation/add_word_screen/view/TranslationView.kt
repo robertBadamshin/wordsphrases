c8b0f77da982848fe134508bb43e567deb946be8
Badamshin Robert <r.badamshin@softpro.com>
2022-07-12 17:27:54 +0300

    add comments

--- android/edit-word-impl/src/main/java/com/app/wordsphrases/edit_word_impl/presentation/add_word_screen/view/TranslationView.kt
+++ android/edit-word-impl/src/main/java/com/app/wordsphrases/edit_word_impl/presentation/add_word_screen/view/TranslationView.kt
@@ -26,11 +26,11 @@ class TranslationView @JvmOverloads constructor(
 
     private val textWatcher = object: TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
+             // do nothing
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
+            // do nothing
         }
 
         override fun afterTextChanged(editable: Editable) {
